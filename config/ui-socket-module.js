a069c1096e5a2b672792b097d2873774ff819753
Napat <napat.charuphant1@gmail.com>
2017-11-24 00:45:38 +0700

    add admin mode

--- config/ui-socket-module.js
+++ config/ui-socket-module.js
@@ -1,7 +1,7 @@
 const $ = require('jquery');
 const alertify = require('alertifyjs');
 const io = require('socket.io-client');
-// var expand = $(".button")
+var admin = "non"
 $(function () {
 // connect to server
 	var socket = io.connect('http://localhost:8080', {'force new connection': true});
@@ -13,7 +13,24 @@ $(function () {
 		console.log(typeof data);
 		if (0 <= data && data <= 199 && typeof data !== 'string') {
 			if (0 == data) {
+				admin = "admin"
 				alertify.success("welcome Admin");
+				var countDownDate = new Date().getTime();
+				var x = setInterval(function() {
+				  // Get todays date and time
+				  var now = new Date().getTime();
+				  // Find the distance between now an the count down date
+				  var distance = now - countDownDate ;
+				  // Time calculations for days, hours, minutes and seconds
+				  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+					console.log(seconds);
+				  // If the count down is finished, write some text
+				  if (seconds > 4) {
+				    clearInterval(x);
+						admin = "non"
+						console.log(admin);
+				  }
+				}, 1000);
 			} else {
 				alertify.success("welcome user : "+data);
 			}
@@ -24,12 +41,21 @@ $(function () {
 		}else {
 			alertify.message(data);
 		}
-
   });
-	$( "#expand" ).on( "click", function() {
+
+	$( ".button-adminMode" ).on( "click", function() {
 		console.log('expand');
-		// expand.addClass(".clicked")
+		if(admin === "admin"){
+			$("#expand").toggleClass('clicked notClick');
+		}
 	});
+
+	$( "#close" ).on( "click", function() {
+		console.log('close');
+    $("#expand").removeClass("clicked");
+    $("#expand").addClass("notClick")
+	});
+
 	$( "#fingerprint" ).on( "click", function() {
 		console.log('check admin');
 		socket.emit('ui_com', "check admin");
