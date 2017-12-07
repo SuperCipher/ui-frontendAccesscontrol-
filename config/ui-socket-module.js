309c6a5946b3df75b2bd108208597dc5c31681a1
Napat <napat.charuphant1@gmail.com>
2017-12-07 23:57:20 +0700

    add delete ui conntroller

--- config/ui-socket-module.js
+++ config/ui-socket-module.js
@@ -1,5 +1,6 @@
 const $ = require('jquery');
 const alertify = require('alertifyjs');
+alertify.set('notifier','position', 'top-right');
 const io = require('socket.io-client');
 var admin = "none"
 var ininterval = 'none'
@@ -7,7 +8,7 @@ $(function () {
 // connect to server
 	var socket = io.connect('http://localhost:8080', {'force new connection': true});
 	// listening to server
-  socket.emit('ui_com', 'CLIENT >>> recieve');
+  socket.emit('ui_com', 'CLIENT >>> standby');
   socket.on('ui_com', function (data) {
 		// print what receive
     console.log(data);
--- public/js/main-ui.js
+++ public/js/main-ui.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
+// const io = require('socket.io-client');
+// var socket = io.connect('http://localhost:8080', {'force new connection': true});
 
-  // Place JavaScript code here...
-  
+
+$(document).ready(function() {
+  // socket.emit('ui_com', 'CLIENT >>> standby');
+
+  $(".btnDel").click(function(){
+    var id = $(this).closest("tr").find('td.fingerId').text();
+    if (confirm('Do you want to delete user: '+id)) {
+
+    }else {
+
+    }
+  });
+  $(".btnBack").click(function(){
+    document.location.href = '/ui';
+  });
 });
