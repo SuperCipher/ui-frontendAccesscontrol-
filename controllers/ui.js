1565911d1186af86737a6fb945e50c2f50691295
Napat <napat.charuphant1@gmail.com>
2017-12-22 02:56:23 +0700

    if ip addr eth0

--- controllers/ui.js
+++ controllers/ui.js
@@ -15,12 +15,12 @@ Object.keys(ifaces).forEach(function (ifname) {
 
     if (alias >= 1) {
       // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
+      // console.log(ifname + ':' + alias, iface.address);
       // ipStream=
     } else {
       // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-      if(ifname=="en0"){
+      // console.log(ifname, iface.address);
+      if(ifname=="eth0"){
         ipStream = iface.address;
         ipStream = "http://"+ipStream+ ":8091"
         // console.log(ipStream);
